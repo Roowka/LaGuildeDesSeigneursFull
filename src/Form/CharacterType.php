1b6177d45ea78375bf963bb8ed3a3c552065358f
Lucas Goï <lucasgoi746@gmail.com>
2023-03-27 11:14:02 +0200

    twig ok

--- src/Form/CharacterType.php
+++ src/Form/CharacterType.php
@@ -6,6 +6,8 @@ use App\Entity\Character;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CharacterType extends AbstractType
 {
@@ -13,9 +15,9 @@ class CharacterType extends AbstractType
     {
         $builder
             ->add('identifier')
-            ->add('name')
-            ->add('kind')
-            ->add('surname')
+            ->add('name', TextType::class)
+            ->add('kind', TextType::class)
+            ->add('surname', TextType::class)
             ->add('caste')
             ->add('knowledge')
             ->add('intelligence')
